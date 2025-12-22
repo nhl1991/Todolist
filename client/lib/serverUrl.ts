5b3be2a48e8934af3909d61ba4ea71361f094ef0
Laslark <laslark1991@gmail.com>
2025-12-22 14:51:31 +0900

    Change Server URL

--- client/lib/serverUrl.ts
+++ client/lib/serverUrl.ts
@@ -1,3 +1,3 @@
 export const SERVER_URL = process.env.NODE_ENV === "development"
     ? "http://localhost:3001"
-    : "https://nestjs-todo-server-0u5r.onrender.com";
+    : "https://todolist-wqa5.onrender.com";
